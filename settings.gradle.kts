ce2bf1863b3bbbd48ff32747413b7737c7ebfe72
Jarvis Semou <jarvis.semou@gmail.com>
2024-02-05 11:27:02 +0800

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ThreadTesterFA"
+include(":app")
+include(":tester")
